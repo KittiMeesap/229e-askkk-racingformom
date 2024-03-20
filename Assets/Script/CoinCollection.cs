d67f32ba83b53445dc2871c60c675cf1e39f97fb
KittiMeesap <tkt.ms@hotmail.com>
2024-03-20 11:58:28 +0700

    Add UI

--- Assets/Script/CoinCollection.cs
+++ Assets/Script/CoinCollection.cs
@@ -14,6 +14,7 @@ public class CoinCollection : MonoBehaviour
         if (other.transform.tag == "Coin")
         {
             Coin++;
+            coinText.text = "Coin: " + Coin.ToString();
             Debug.Log(Coin);
             Destroy(other.gameObject);
         }
